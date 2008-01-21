467899bb867db9006d067a09dbc641102c2762a4
javacat <javacat@ef5aec9b-4416-0410-8bd8-d51effda29bf>
2008-01-21 12:45:24 +0000

    modified the script file, it now can both work with develop mode and release

--- jastrology.sh
+++ jastrology.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
-JASTROLOGY_HOME=.
+JASTROLOGY_HOME=$INSTALL_PATH
+if [ -z "$JASTROLOGY_HOME" ]; then
+	JASTROLOGY_HOME=.
+fi
 MAIN_CLASS=com.ivstars.astrology.gui.Chart
-TMP_CP=$JASTROLOGY_HOME/target/build
+if [ -f "$JASTROLOGY_HOME/classes" ]; then
+	TMP_CP=$JASTROLOGY_HOME/classes
+else
+	TMP_CP=$JASTROLOGY_HOME/target/build
+fi
 for i in "$JASTROLOGY_HOME"/lib/*.jar; do
 	TMP_CP="$TMP_CP":"$i"
 done
-java -Xmx256m  -cp $TMP_CP $MAIN_CLASS --jastrology-home $JASTROLOGY_HOME $*
+java -Xmx256m  -cp $TMP_CP $MAIN_CLASS --jastrology-home $JASTROLOGY_HOME $@
 
